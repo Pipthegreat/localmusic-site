c65be424e48628024c6f7a05273f7db87d04a9ac
Raleigh <mr.raleigh.j@gmail.com>
2026-05-05 18:28:37 -0700

    Auto-update companion HTML (2026-05-05 18:28)

--- functions/api/logout.js
+++
@@ -1,7 +0,0 @@
-import { COOKIE_NAME, makeCookieHeader, json } from '../_utils.js';
-
-export async function onRequestPost() {
-  return json({ ok: true }, {
-    headers: { 'Set-Cookie': makeCookieHeader(COOKIE_NAME, '', 0) }
-  });
-}
